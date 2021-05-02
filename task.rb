f6d1441af81be985073ffa6c28546946f8fcd767
HiroyukiOgasawara <hiroyukiogasawara@ogasawarahiroyukinoMacBook-Pro.local>
2021-05-02 11:00:37 +0900

    add subject 4 to 6

--- task.rb
+++ task.rb
@@ -29,6 +29,7 @@ def q4
   sports = ["サッカー", "フットサル", nil, "野球", "バスケ", nil, "バレー"]
 
   # 以下に回答を記載
+  sports.delete(nil)
 
   # 以下は変更しないで下さい
   p sports
@@ -39,14 +40,21 @@ def q5
   array2 = [1, 5, 8, 10]
 
   # 以下に回答を記載
-
+  puts "array1出力結果"
+  p array1.empty?
+  puts "\n"
+  puts "array2出力結果"
+  p array2.empty?
 end
 
 def q6
   numbers1 = [1, 2, 3, 4, 5]
 
   # 以下に回答を記載
-
+  numbers2 = numbers1.map do |num|
+    num * 10
+  end
+  p numbers2
 end
 
 def q7
