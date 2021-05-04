cad71dbcce953de6653f006dc7a562b5838a37e8
HiroyukiOgasawara <hiroyukiogasawara@ogasawarahiroyukinoMacBook-Pro.local>
2021-05-04 15:59:54 +0900

    add subject 7 to 9

--- task.rb
+++ task.rb
@@ -64,6 +64,7 @@ def q7
   array = ["1", "2", "3", "4", "5"]
 
   # 以下に回答を記載
+  array.map!(&:to_i)
 
   # 以下は変更しないで下さい
   p array
@@ -73,6 +74,8 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
+  programming_languages.map!(&:capitalize)
+  upper_case_programming_languages = programming_languages.map(&:upcase)
 
   # 以下は変更しないで下さい
   p programming_languages
@@ -83,7 +86,9 @@ def q9
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
-
+  names.each.with_index(1) do |name, i|
+    puts "会員No.#{i} #{name}さん"
+  end
 end
 
 def q10
